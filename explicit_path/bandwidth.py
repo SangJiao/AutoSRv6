6e317aa6885333dbb2edd81adf031c4a69981964
L <lbcdxx@163.com>
2022-07-22 23:39:02 +0800

    1

--- explicit_path/bandwidth.py
+++ explicit_path/bandwidth.py
@@ -11,21 +11,26 @@ Output: 求解src到dst满足带宽约束的显示路径
 import networkx as nx
 import z3
 
-pol = [['a', 'd', 200], ['b', 'd', 1000]]
-
-g = nx.DiGraph()
-g.add_edge('a', 'b')
-g.add_edge('b', 'd')
-g.add_edge('a', 'c')
-g.add_edge('c', 'd')
+# pol = [['a', 'd', 200], ['b', 'd', 1000]]
+#
+# g = nx.DiGraph()
+# g.add_edge('a', 'b')
+# g.add_edge('b', 'd')
+# g.add_edge('a', 'c')
+# g.add_edge('c', 'd')
+#
+# for ner in g['c'].items():
+#     print(ner)
 
 
 class BandWidth:
     def __init__(self, topo, policy):
         assert (isinstance(topo, nx.DiGraph))
         self.topo = topo
+        self.init_policy = policy
         self.policy = {}
-        for list in policy:
+        # for list in policy:
+        for list in policy.paths:
             key = (list[0], list[1])
             self.policy[key] = list[2]
         self.edge_key_var = {}  #key： 四元组(topo.edge,policy.key) value: z3 var
@@ -77,13 +82,42 @@ class BandWidth:
                 vars_sum += z3.If(tem_z3_var == True, pol_need, 0)
             self.solver.add(vars_sum <= max_edge_bandwidth)
 
+    def set_paths(self):
+        assert self.solver.check()
+        model = self.solver.model()
+        new_paths = []
+        # for list in self.init_policy:
+        for list in self.init_policy.paths:
+            start_node = list[0]
+            end_node = list[1]
+            tem_list = []
+            cur_node = start_node
+            tem_list.append(cur_node)
+            while cur_node != end_node:
+                for ner, value in self.topo[cur_node].items():
+                    tem_tuple = (cur_node, ner, start_node, end_node)
+                    if bool(model.get_interp(self.edge_key_var[tem_tuple])):
+                        cur_node = ner
+                        tem_list.append(cur_node)
+                        break
+            new_paths.append(tem_list)
+        # print(new_paths)
+        self.init_policy.paths = new_paths
 
 
 
-k = BandWidth(g, pol)
-k.creat_z3_var()
-k.path_link_con()
-k.edge_bandwidth_con()
-k.solver.check()
-ppp = k.solver.model()
-print(ppp)
+
+# k = BandWidth(g, pol)
+# k.creat_z3_var()
+# k.path_link_con()
+# k.edge_bandwidth_con()
+# k.solver.check()
+# ppp = k.solver.model()
+# for i in k.edge_key_var.values():
+#     print(ppp.get_interp(i))
+#     print(bool(ppp.get_interp(i)))
+#
+# print(ppp)
+# print(ppp.get_interp(k.edge_key_var[('c','d','a','d')]))
+#
+# k.set_paths()
