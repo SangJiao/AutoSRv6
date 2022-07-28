96884fbd2f04d93b28ff2908857f0a09ad41ccac
L <lbcdxx@163.com>
2022-07-28 10:51:58 +0800

    1

--- topo_complete.py
+++ topo_complete.py
@@ -18,22 +18,30 @@ def set_interface_ipv6(topo):
     assert (isinstance(topo, nx.DiGraph))
     loop_sid_ipv6_change_state = 1
     for node in topo.nodes:
-        topo.nodes[node][keyword.IP] = IP("2001:DD8:"+hex(loop_sid_ipv6_change_state).upper()+"::1/128")
+        topo.nodes[node][keyword.LOOPBACK1] = IP("2001:DD8:"+hex(loop_sid_ipv6_change_state).upper()+"::1/128")
         topo.nodes[node][keyword.PREFIX_SID] = IP('2001:DA8:'+hex(loop_sid_ipv6_change_state)[2:].upper()+'::1/128')
+        topo.edges[node]['network-entity'] = "10.0000.0000."+int2str(loop_sid_ipv6_change_state)+".00"
         loop_sid_ipv6_change_state += 1
     dir = {}
-    inter_ipv6_change_state = 1
-    xsid_ipv6_change_state = 1
+    xsid_dir = {}
+    inter_xsid_ipv6_change_state = 1
     for edge in topo.edges:#(A,B)
         if edge not in dir.keys():
-            topo.edges[edge][keyword.INTERFACE] = IP('2001:DB8:'+hex(inter_ipv6_change_state)[2:].upper()+'::1')  #(A,B)    A_INTERFACE[]
+            topo.edges[edge][keyword.INTERFACE] = IP('2001:DB8:'+hex(inter_xsid_ipv6_change_state)[2:].upper()+'::1')  #(A,B)    A_INTERFACE[]
             topo.edges[edge]['mask'] = 64
+            topo.edges[edge][keyword.ADJ_SID] = IP('2001:D08:'+hex(inter_xsid_ipv6_change_state)[2:].upper()+'::1/128')
             tem1 = (edge[1], edge[0]) #(B,A)
             # topo.edges[edge][keyword.INTERFACE] = IP('2001:DB8:' + hex(inter_ipv6_change_state)[2:].upper() + '::2')
-            dir[tem1] = IP('2001:DB8:'+hex(inter_ipv6_change_state)[2:].upper()+'::2')
-            inter_ipv6_change_state += 1
+            dir[tem1] = IP('2001:DB8:'+hex(inter_xsid_ipv6_change_state)[2:].upper()+'::2')
+            xsid_dir[tem1] = IP('2001:D08:'+hex(inter_xsid_ipv6_change_state)[2:].upper()+'::2/128')
+            inter_xsid_ipv6_change_state += 1
         else:
             topo.edges[edge][keyword.INTERFACE] = dir[edge]
             topo.edges[edge]['mask'] = 64
-        topo.edges[edge][keyword.ADJ_SID] = IP('2001:D08:'+str(hex(xsid_ipv6_change_state)[2:]).upper()+'::1/128')
-        xsid_ipv6_change_state += 1
+            topo.edges[edge][keyword.ADJ_SID] = xsid_dir[edge]
+        # topo.edges[edge][keyword.ADJ_SID] = IP('2001:D08:'+str(hex(xsid_ipv6_change_state)[2:]).upper()+'::1/128')
+        # xsid_ipv6_change_state += 1
+
+
+def int2str(i):
+    return "0"*(4-len(str(i)))+str(i)
